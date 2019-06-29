3cd7da0813b2905e30333a2657a67a879c5d4fd6
Gabriel Cossette <gabriel@gcossette.com>
2019-06-29 11:08:38 -0400

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+
+#=================================================
+# COMMON VARIABLES
+#=================================================
+
+# dependencies used by the app
+
+extra_pkg_dependencies="php7.2-fpm php7.2-cli php7.2-gd php7.2-mysql php7.2-xml php7.2-ldap php7.2-mbstring php7.2-json php7.2-simplexml php7.2-curl"
