6c1bdc2928eaa5a23e98e34c16d773883897cc3b
TomRicci <44097104+TomRicci@users.noreply.github.com>
2020-02-08 15:24:20 +0100

    Update _common.sh
    php7.2-zip

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,4 +6,4 @@
 
 # dependencies used by the app
 
-extra_pkg_dependencies="php7.2-fpm php7.2-cli php7.2-gd php7.2-mysql php7.2-xml php7.2-ldap php7.2-mbstring php7.2-json php7.2-simplexml php7.2-curl php7.2-mbstring php7.2-ctype"
+extra_pkg_dependencies="php7.2-fpm php7.2-cli php7.2-gd php7.2-mysql php7.2-xml php7.2-ldap php7.2-mbstring php7.2-json php7.2-simplexml php7.2-curl php7.2-mbstring php7.2-ctype php7.2-zip"
